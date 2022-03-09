c06d8a24f013412023bce8fcf44577a7e7da0918
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-03-09 13:59:45 +0000

    - initial tests

---
+++ QRCodeEncoder/QRCodeEncoder.Core.Tests/QREncoder_Tests.cs
@@ -0,0 +1,43 @@
+namespace QRCodeEncoder.Core.Tests;
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+public class QREncoder_Tests
+{
+  [Test]
+  public void Constructor_Succeeds()
+  {
+    Action act = () => Create();
+
+    act.Should().NotThrow();
+  }
+
+  [Test]
+  public void ECIAssignValue_InRange_Succeeds(
+    [Values(-1, 0, 1, 10, 100, 1000, 10000, 100000, 999999)] int val)
+  {
+    var qrenc = Create();
+
+    qrenc.ECIAssignValue = val;
+
+    qrenc.ECIAssignValue.Should().Be(val);
+  }
+
+  [Test]
+  public void ECIAssignValue_OutOfRange_Throws(
+    [Values(-10, -2, 1000000)] int val)
+  {
+    var qrenc = Create();
+
+    Action act = () => qrenc.ECIAssignValue = val;
+
+    act.Should().Throw<Exception>();
+  }
+
+  private QREncoder Create()
+  {
+    return new QREncoder();
+  }
+}
