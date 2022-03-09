fcac0206bf4a9c886375d13ac1096f77cb5116e0
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-03-09 16:07:31 +0000

    - refactored

--- QRCodeEncoder/QRCodeEncoder.Core.Tests/QREncoder_Tests.cs
+++ QRCodeEncoder/QRCodeEncoder.Core.Tests/QREncoder_Tests.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-public class QREncoder_Tests
+public sealed class QREncoder_Tests
 {
   [Test]
   public void Constructor_Succeeds()
